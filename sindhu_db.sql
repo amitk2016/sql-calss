066361409af0dd5b85ea3430cdcb24e0c67bae45
Sindhu Sankarankutty <sindhu.sankarankutty@yoobee.ac.nz>
2016-06-16 11:57:24 +1200

    sql dump after updations

--- sindhu_db.sql
+++ sindhu_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 10, 2016 at 02:20 am
+-- Generation Time: Jun 16, 2016 at 01:56 am
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -28,19 +28,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `movies` (
 `id` int(11) unsigned NOT NULL,
-  `title` varchar(10) NOT NULL,
+  `title` varchar(15) NOT NULL,
   `description` text NOT NULL,
   `rating` enum('PGR','R','M','G') DEFAULT 'G',
   `duration` decimal(5,0) NOT NULL,
   `release_date` year(4) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `movies`
 --
 
 INSERT INTO `movies` (`id`, `title`, `description`, `rating`, `duration`, `release_date`) VALUES
-(4, 'Frozen', 'hefkehrkhdakfhksdhfkhsdkfhkshdfkhsdkfhksdhfkshdfkhsdkfhksdhf', 'G', '127', 2016);
+(6, 'Rio 2', 'kdkgskdfgksgdfgsdkf', 'PGR', '100', 2015),
+(7, 'Inside Out', 'After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.', 'G', '95', 2015),
+(8, 'Hugo', 'Asa Butterfield is brilliant as the twelve years old boy who lives alone in the walls of 1930s railway station in Paris and steals food to get by. Torn from a loving watchmaker father upon his death, and forced to repair clocks for his alcoholic uncle, Hugo devotes his time to working on his father’s last project: repairing the automaton, a mechanical man who can write. Desperate to figure out if his father left him a message in the automaton, he adopts dubious measures to obtain the parts he needs. It was during such a theft from a toy shop that he meets the owner’s daughter, Isabelle, who decides to help him in his project. Together they go on an adventure with a key that will reveal to them a secret from a magician’s past. Adapted from the book, The Invention of Hugo Cabaret, Martin Scorsese brings innocence to the elegant narrative.', 'G', '100', 2011);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +62,7 @@ ALTER TABLE `movies`
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
